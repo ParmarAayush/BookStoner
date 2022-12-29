30b245977f21a0d07980e194815e8a258215613c
Parmar Aayush <parmarayush1816@gmail.com>
2022-12-29 19:06:55 +0530

    complete task when user logdin show profile not then show signin

--- javaScript/profile.js
+++ javaScript/profile.js
@@ -1,12 +1,17 @@
-let logedIn = 1;
+let logedIn = 0;
 let profile = document.getElementById("profileActive");
 
-profile.addEventListener("click", function regSignin(e) {
+document.addEventListener("DOMContentLoaded", function regSignin(e) {
     e.preventDefault();
 
     if (logedIn == true) {
         document.getElementsByClassName("register")[0].classList.add("displaynone");
         document.getElementsByClassName("signIn")[0].classList.add("displaynone");
+    }else
+    {
+        document.getElementsByClassName("tempOne")[0].classList.add("displaynone");
+        document.getElementsByClassName("register")[0].classList.add("displaynone");
+        document.getElementsByClassName("signIn")[0].classList.remove("displaynone");
     }
 })
 
