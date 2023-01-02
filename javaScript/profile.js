03e35e6023f1bf5d815dca6ffcda4524c4b1cd48
Parmar Aayush <parmarayush1816@gmail.com>
2023-01-02 18:55:13 +0530

    add log in function backend

--- javaScript/profile.js
+++ javaScript/profile.js
@@ -2,7 +2,7 @@ import * as firebase from "./firebase.js"; // configure using import to utilize 
 
 // firebase import
 
-import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
 import { getFirestore, setDoc, doc } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-firestore.js";
 
 // creat variable to use services 
@@ -15,18 +15,21 @@ const db = getFirestore();
 let logedIn = false;
 let profile = document.getElementById("profileActive");
 
-document.addEventListener("DOMContentLoaded", function regSignin(e) {
-    e.preventDefault();
-
+function regSignin() {
+    // e.preventDefault();
+    console.log("hide functionality work");
     if (logedIn == true) {
         document.getElementsByClassName("register")[0].classList.add("displaynone");
         document.getElementsByClassName("signIn")[0].classList.add("displaynone");
+        document.getElementsByClassName("tempOne")[0].classList.remove("displaynone")
     } else {
         document.getElementsByClassName("tempOne")[0].classList.add("displaynone");
         document.getElementsByClassName("register")[0].classList.add("displaynone");
         document.getElementsByClassName("signIn")[0].classList.remove("displaynone");
     }
-})
+}
+
+document.addEventListener("DOMContentLoaded", regSignin);
 
 document.getElementById("signinLink").addEventListener("click", function () {
     document.getElementsByClassName("register")[0].classList.add("displaynone");
@@ -64,7 +67,7 @@ document.getElementById("registerBtn").addEventListener("click", function () {
             // Signed in 
             const user = userCredential.user;
             console.log(user.uid);
-            alert("Registration Successfull");
+            alert("Registration Successfully");
 
             setDoc(doc(db, "User", email), {
                 Name: name,
@@ -81,4 +84,27 @@ document.getElementById("registerBtn").addEventListener("click", function () {
             console.log(errorCode);
             console.log(errorMessage);
         });
+})
+
+document.getElementById("signinBtn").addEventListener("click", function () {
+
+    let email = document.getElementById("signInemail").value;
+    let password = document.getElementById("signInPassword").value;
+
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            alert("Log In SuccessFully");
+            console.log(userCredential.user.uid);
+            logedIn = userCredential.user.uid != null ? true : false;
+            regSignin();
+            console.log(logedIn);
+            // location.replace("/BookStoner/index.html");
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorCode);
+            console.log(errorMessage);
+        });
+        // [error] regSignin(e); its give error resolve it 
 })
