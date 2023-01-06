0259d5c72aee3e4194a740b09236689d0f33bda3
Parmar Aayush <parmarayush1816@gmail.com>
2023-01-06 09:00:16 +0530

    add upload fields data to the database but error in uploadTas.on

--- javaScript/upload.js
+++ javaScript/upload.js
@@ -1,11 +1,14 @@
 import * as firebase from "./firebase.js"; // configure using import to utilize code 
 
-alert("Upload javaScript Work")
+// alert("Upload javaScript Work")
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
 import { getFirestore, collection, setDoc, doc, addDoc } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-firestore.js";
+import { getStorage, ref, uploadBytesResumable, getDownloadURL, uploadBytes } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-storage.js";
+
 
 const auth = getAuth();
 const db = getFirestore();
+const storage = getStorage();
 const user = auth.currentUser;
 let userEmail;
 let logedIn;
@@ -24,9 +27,46 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 })
+document.getElementById("coverpage").addEventListener("change", function uploadImage(e) {
+    alert("Upload Function is Work");
+    try {
+        console.log(e.target.files[0]);
+        const fileName = e.target.files[0];
+        var name = fileName.name.split('.').shift() + Math.floor(Math.random() * 10);
+        var type = fileName.type.split("/")[0];
+        var path = type + '/' + name;
+        console.log(path)
+        const storageRef = ref(storage, path);
+        const uploadTask = uploadBytes(storageRef, e.target.files[0]);
+        uploadTask.on('state_changed', (snapshot) => {
+            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log('Upload is ' + progress + '% done');
+            switch (snapshot.state) {
+                case 'paused':
+                    console.log('Upload is paused');
+                    break;
+                case 'running':
+                    console.log('Upload is running');
+                    break;
+            }
+        },
+            (error) => {
+            },
+            () => {
+                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                    console.log('File available at', downloadURL);
+                });
+            }
+        );
+    } catch (error) {
+        console.log(error)
+        alert(error);
+    }
+})
+
 document.getElementById("upload").addEventListener("click", async function () {
 
-    
+
     console.log(userEmail);
     let title = document.getElementById("title").value;
     let author = document.getElementById("author").value;
